7eeffd9f6f6f711d0b01ee93bdf2c011e1b0b593
Yusuke Nagata <59643091+E-yusuke@users.noreply.github.com>
2022-10-31 16:14:03 +0900

    Update jupyter.sh

--- docker/jupyter.sh
+++ docker/jupyter.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -itd my_docker jupyter-lab --no-browser --port=8888 --ip=0.0.0.0 --allow-root --NotebookApp.token=''
+docker exec -itd T3_docker jupyter-lab --no-browser --port=8888 --ip=0.0.0.0 --allow-root --NotebookApp.token=''
